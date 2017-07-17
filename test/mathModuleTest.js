14fb8c05426cad32533961ceb2a0577770260402
KratiJain <kratij@thoughtworks.com>
2017-07-17 18:42:42 +0530

    Adding Math module tests to be implemented

---
+++ test/mathModuleTest.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+
+var test_addition_of_two_values = function () {
+    var sum = mathModule.add(1, 3);
+    assert.equal(4, sum);
+}();
+
+var test_multiplication_of_two_values = function () {
+    var product = mathModule.multiply(10, 5);
+    assert.equal(50, product);
+}();
+
+
