1d6498babc2373b41d66890312f29840975aab1b
KratiJain <kratij@thoughtworks.com>
2017-07-18 11:32:28 +0530

    creating links to users page from home page and vice-versa

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ var fs = require('fs');
 var RESOURCES_PATH = './resources/users.json';
 
 var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "jade");
 
-app.get('/', function (req, res, next) {
+app.get('/', function (req, res) {
     res.render("index");
 });
 
