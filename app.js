45a53942e6c251de196ac59e90c49554839098cd
KratiJain <kratij@thoughtworks.com>
2017-07-18 12:50:39 +0530

    Adding route for /users and body parser to parse json and url

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var fs = require('fs');
 
 var RESOURCES_PATH = './resources/users.json';
@@ -7,11 +8,20 @@ var RESOURCES_PATH = './resources/users.json';
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "jade");
 
 app.get('/', function (req, res) {
     res.render("index");
+});
+
+app.get('/users', function (req, res) {
+    var fileContents = fs.readFileSync(RESOURCES_PATH, 'utf-8');
+    var users = JSON.parse(fileContents);
+    res.render("users", {layout: false, users: users});
 });
 
 
