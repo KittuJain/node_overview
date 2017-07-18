bad7361b921fcf3fafa4c03074b3417bc4401555
KratiJain <kratij@thoughtworks.com>
2017-07-18 11:18:54 +0530

    Rendering home page and adding resources for users

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
+
+var RESOURCES_PATH = './resources/users.json';
 
 var app = express();
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "jade");
+
+app.get('/', function (req, res, next) {
+    res.render("index");
+});
+
 
 module.exports = app;
